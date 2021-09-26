830962a38f013c82d755484da347a344941eba32
wyt2422762 <724752500@qq.com>
2021-09-26 09:22:29 +0800

    初始化

---
+++ src/main/java/com/fdkj/ysps/YspsApplication.java
@@ -0,0 +1,13 @@
+package com.fdkj.ysps;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YspsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YspsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/fdkj/ysps/YspsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fdkj.ysps;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YspsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
